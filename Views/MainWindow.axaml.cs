4e85471a85441945621cb8350be92f75a49242a6
IvanKarezin <ivan.karezin@gmail.com>
2025-01-04 16:44:58 +0300

    fill main window and add modal

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,20 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace TSManager.Views;
 
 public partial class MainWindow : Window
 {
+    private readonly AddWindow _addWindow;
     public MainWindow()
     {
         InitializeComponent();
+        this._addWindow = new AddWindow();
+    }
+
+    private async void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var result = await this._addWindow.ShowDialog<string>(this);
+        this._addWindow.Show();
     }
 }
